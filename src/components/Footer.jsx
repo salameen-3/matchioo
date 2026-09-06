3fa01234f2b811630eb62c3353df2cc14e31feaa
salameen-3 <salameenyones@gmail.com>
2026-09-06 16:55:09 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,8 @@ const SOCIAL = [
   },
   {
     name: 'LinkedIn',
-    href: 'https://www.linkedin.com/',
+    href: 'https://www.linkedin.com/in/sumoud-salamin-20aaa82b2?utm_source=share_via&utm_content=profile&utm_medium=member_android
+',
     svg: (
       <svg width="26" height="26" viewBox="0 0 26 26" aria-hidden="true">
         <path d="M 23.111 0 C 24.707 0 26 1.293 26 2.889 L 26 23.111 C 26 24.707 24.707 26 23.111 26 L 2.889 26 C 1.293 26 0 24.707 0 23.111 L 0 2.889 C 0 1.293 1.293 0 2.889 0 Z M 22.389 22.389 L 22.389 14.733 C 22.389 12.133 20.281 10.024 17.68 10.024 C 16.452 10.024 15.022 10.776 14.329 11.902 L 14.329 10.299 L 10.299 10.299 L 10.299 22.389 L 14.329 22.389 L 14.329 15.268 C 14.329 14.156 15.224 13.246 16.337 13.246 C 17.454 13.246 18.359 14.151 18.359 15.268 L 18.359 22.389 Z M 5.604 8.031 C 6.945 8.031 8.031 6.945 8.031 5.604 C 8.031 4.261 6.948 3.163 5.604 3.163 C 4.256 3.163 3.163 4.256 3.163 5.604 C 3.163 6.948 4.261 8.031 5.604 8.031 M 7.612 22.389 L 7.612 10.299 L 3.611 10.299 L 3.611 22.389 Z" />
@@ -31,7 +32,7 @@ const SOCIAL = [
   },
   {
     name: 'Instagram',
-    href: 'https://www.instagram.com/',
+    href: 'https://www.instagram.com/salaminsumoud/',
     svg: (
       <svg width="26" height="26" viewBox="0 0 26 26" aria-hidden="true">
         <path d="M 7.54 0 L 18.46 0 C 22.62 0 26 3.38 26 7.54 L 26 18.46 C 26 22.624 22.624 26 18.46 26 L 7.54 26 C 3.38 26 0 22.62 0 18.46 L 0 7.54 C 0 3.376 3.376 0 7.54 0 M 7.28 2.6 C 4.695 2.6 2.6 4.695 2.6 7.28 L 2.6 18.72 C 2.6 21.307 4.693 23.4 7.28 23.4 L 18.72 23.4 C 21.305 23.4 23.4 21.305 23.4 18.72 L 23.4 7.28 C 23.4 4.693 21.307 2.6 18.72 2.6 Z M 19.825 4.55 C 20.722 4.55 21.45 5.278 21.45 6.175 C 21.45 7.072 20.722 7.8 19.825 7.8 C 18.928 7.8 18.2 7.072 18.2 6.175 C 18.2 5.278 18.928 4.55 19.825 4.55 M 13 6.5 C 16.59 6.5 19.5 9.41 19.5 13 C 19.5 16.59 16.59 19.5 13 19.5 C 9.41 19.5 6.5 16.59 6.5 13 C 6.5 9.41 9.41 6.5 13 6.5 M 13 9.1 C 10.846 9.1 9.1 10.846 9.1 13 C 9.1 15.154 10.846 16.9 13 16.9 C 15.154 16.9 16.9 15.154 16.9 13 C 16.9 10.846 15.154 9.1 13 9.1" />
@@ -78,13 +79,13 @@ export default function Footer() {
       <div className="ft-links">
         <div className="ft-info">
           <h4>Matchioo &nbsp;Matcha Cafe</h4>
-          <a href="https://maps.google.com/?q=99+Bay+MM.,+Peters,+ON+M5K+994,+London" target="_blank" rel="noopener noreferrer">
-            99 Bay MM., Peters, ON M5K 994, London
+          <a href="https://share.google/3pqChHP5fBm2tGRvy" target="_blank" rel="noopener noreferrer">
+            Delivery across the West Bank, Jerusalem and inside
           </a>
           <div className="ft-contact">
             <h4>Contact</h4>
             <a href="mailto:hello@matchioo.com">hello@matchioo.com</a>
-            <a href="tel:+8801234567890">+880 1234 567 890</a>
+            <a href="tel:+972 593 604 96">+972 593 604 96</a>
           </div>
         </div>
 
